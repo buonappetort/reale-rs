73438de93e082eb46d7ee215e8cf58dd56e8b3f9
Anthony Tortorello <tortorello.anthony@gmail.com>
2022-04-03 13:46:45 -0400

    feat: total monthly payment attribute and calculation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::collections::{HashMap};
-use std::convert::{TryInto};
+use std::collections::HashMap;
+use std::convert::TryInto;
 
 const MONTHLY_RENT: i32 = 2500;
 
@@ -7,59 +7,75 @@ struct AmmortizationPeriod {
     balance_start: f64,
     interest: f64,
     balance_end: f64,
-    principal: f64
+    principal: f64,
 }
 struct Ammortization {
     annual_interest_rate: f64,
     periods: i32,
-    schedule: HashMap<i32, AmmortizationPeriod>
+    total_monthly_payment: f64,
+    loan_amount: f64, // Should this be stored? Or just live
+                      // as a method...
+                      //schedule: HashMap<i32, AmmortizationPeriod>
 }
 
 impl Ammortization {
-    fn new(&mut self, annual_interest_rate: f64, periods: i32, principal: f64) -> Ammortization {
+    fn new(annual_interest_rate: f64, periods: i32, loan_amount: f64) -> Ammortization {
+        // TODO: OH BOY TIME TO IMPLEMENT SOME MATH!!!
+        // How to calculate a monthly mortgage payment (TMP)
+        // TMP = ( P * ( r * ( ( 1 + r )^n ) ) ) / ( ( (1 + r)^n ) - 1 );
+        let P = loan_amount.clone();
+        let r = annual_interest_rate / 12.0;
+        let n = periods * 12;
+        let total_monthly_payment =
+            (P * (r * ((1.0 + r).powf(n.into())))) / (((1.0 + r).powf(n.into())) - 1.0);
         Ammortization {
             annual_interest_rate,
             periods,
-            schedule: self.generate_schedule(&principal, &annual_interest_rate, &periods)
+            total_monthly_payment,
+            loan_amount,
         }
     }
 
-    fn generate_schedule(&mut self, annual_interest_rate: &f64, periods: &i32, principal: &i32) -> HashMap<i32, AmmortizationPeriod> {
-        
+    fn generate_schedule(
+        &mut self,
+        _principal: &f64,
+        _annual_interest_rate: &f64,
+        periods: &i32,
+    ) -> HashMap<i32, AmmortizationPeriod> {
         // hashmap of length = periods
-        let schedule = HashMap::with_capacity(periods.clone().try_into().unwrap());
+        HashMap::with_capacity(periods.clone().try_into().unwrap())
 
         // TODO: OH BOY TIME TO IMPLEMENT SOME MATH!!!
         // How to calculate a monthly mortgage payment (TMP)
         // TMP = ( P * ( r * ( ( 1 + r )^n ) ) ) / ( ( (1 + r)^n ) - 1 );
-        let P = principal.clone();
-        let r = annual_interest_rate / 12.0;
-        let n = 30 * 12;
-        let monthly_payment = ( P * ( r * ( ( 1 + r )^n ) ) ) / ( ( (1 + r)^n ) - 1 );
-        
+        // let P = principal.clone();
+        // let r = annual_interest_rate / 12.0;
+        // let n = 30 * 12;
+        // let monthly_payment = ( P * ( r * ( ( 1 + r )^n ) ) ) / ( ( (1 + r)^n ) - 1 );
 
         // assumming annualized
-        // n            balance_start                  interest                principal                           balance_end        
-        // 1            Bs = principal                 r * balance_start       monthly_payment - interest          Be[1] = balance_start - principal 
+        // n            balance_start                  interest                principal                           balance_end
+        // 1            Bs = principal                 r * balance_start       monthly_payment - interest          Be[1] = balance_start - principal
         // 2            Bs = B[n-1]                                                                                    Be[2] =
         // 3                                                                                                Be[3]
         // 4
         // 5
         // ...
         // periods                                                                                          Be[n]
-
     }
 }
 
-fn calculate_expenses() -> f64 {
+fn calculate_expenses() -> () {
     // inputs
-    let principal: f64 = 500.0;
-    let interest_rate: f64 = 0.05;  
+    let loan_amount: f64 = 200000.00;
+    let annual_interest_rate: f64 = 0.05;
 
+    let ammortization = Ammortization::new(annual_interest_rate, 30, loan_amount);
 
-
-    principal
-
+    print!(
+        "Your total monthly payment is: {:?}",
+        ammortization.total_monthly_payment
+    );
 }
 
 fn main() {
@@ -71,4 +87,15 @@ fn main() {
     let income = MONTHLY_RENT.clone();
 
     println!("Monthly Gross Income: {}", &income);
+}
+
+#[test]
+fn test1() {
+        // inputs
+        let loan_amount: f64 = 200000.00;
+        let annual_interest_rate: f64 = 0.05;
+    
+        let ammortization = Ammortization::new(annual_interest_rate, 30, loan_amount);
+    
+        assert_eq!(ammortization.total_monthly_payment.ceil(), 1074.00)
 }
